be6e775cb733424cc33e8cea7e329ab032b454f0
SGSm <uwe-me@arcor.de>
2021-01-03 03:43:57 +0100

    Nothing changed

---
+++ CMakeFiles/paleofetch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paleofetch.dir/src/paleofetch.c.o"
+  "paleofetch"
+  "paleofetch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/paleofetch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
